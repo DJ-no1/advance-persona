0e24e2df918303b389e2dcb35c92c69637d54dc1
DJ-no1 <darkjargon739@gmail.com>
2025-07-11 11:52:41 +0530

    response check

--- persona/persona.py
+++ persona/persona.py
@@ -1,15 +1,19 @@
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate,load_prompt
+
+load_dotenv()
 
 # Initialize Google Generative AI chat model
-llm = GoogleGenerativeAI(model="gemini-2.0-flash" )
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash" )
 
 # Define the prompt template
 
-prompt = ("../prompts/girl_persona.json")
-
-response = llm.generate(prompt)
+prompt = load_prompt("prompts/girl_persona.json")
 
 userinput = "Hello, how are you?"
-chain = 
+chain = prompt | llm
 
-response = llm.generate(userinput)
+result = chain.invoke({"userinput": userinput})
+
+print(result.content)
